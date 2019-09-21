b64825856b7d6db33a19f329184d5aa49a6ba96d
ASzwarc <szwarc.adam@gmail.com>
2019-09-21 15:54:40 +0200

    feature: added plotting of image using matplotlib.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import cv2 as cv
+from matplotlib import pyplot as plt
 
 def main(filename: str) -> None:
     img = cv.imread(filename, cv.IMREAD_GRAYSCALE)
     if img is not None:
         print(f"Image {filename} successfuly read!")
+        plot_image(img)
     else:
         print(f"Couldn't open image {filename} !")
+
+def plot_image(image) -> None:
+    plt.imshow(image, cmap='gray')
+    plt.show()
 
 if __name__ == "__main__":
     main("input_images/SimpleBoard_1.PNG")
