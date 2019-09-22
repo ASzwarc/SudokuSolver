d6e2001b963bb26264e5f252f0a4a7285e8c610d
ASzwarc <szwarc.adam@gmail.com>
2019-09-22 21:07:03 +0200

    feature: finding boundaries of the sudoku board.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import cv2 as cv
 from matplotlib import pyplot as plt
+import numpy as np
 
 def main(filename: str) -> None:
     img_gray = cv.imread(filename, cv.IMREAD_GRAYSCALE)
     img_color = cv.imread(filename, cv.IMREAD_UNCHANGED)
     if img_gray is not None and img_color is not None:
         print(f"Image {filename} successfuly read!")
-        plot_image(find_lines(threshold_image(img_gray), img_color))
+        plot_image(find_lines(img_gray, img_color))
     else:
         print(f"Couldn't open image {filename} !")
 
@@ -16,9 +17,23 @@ def threshold_image(image):
                                 cv.THRESH_BINARY, 11, 2)
 
 def find_lines(image_grayscale, image_color):
-    lines = cv.HoughLinesP(image_grayscale, 1, 3.14/180.0, 100, 100, 10)
-    for x1, y1, x2, y2 in lines[0]:
-        print(f"Found lines: ({x1}, {y1}) - ({x2}, {y2})")
+    edges = cv.Canny(image_grayscale, 200, 220, apertureSize=3)
+    lines = cv.HoughLinesP(edges, 1, np.pi/180.0, 200, 300, 4)
+    # TODO this should be sorted and copied to another list
+    found_lines = []
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        found_lines.append((x1, y1, x2, y2))
+    found_lines.sort()
+    board_boundaries = []
+    board_boundaries.append(found_lines[0])
+    board_boundaries.append(found_lines[-1])
+    found_lines.sort(key=lambda points: points[3])
+    board_boundaries.append(found_lines[0])
+    board_boundaries.append(found_lines[-1])
+    print(board_boundaries)
+    for boundary in board_boundaries:
+        x1, y1, x2, y2 = boundary
         cv.line(image_color, (x1, y1), (x2, y2), (0, 255, 0), 2)
     return cv.cvtColor(image_color, cv.COLOR_BGR2RGB)
 
