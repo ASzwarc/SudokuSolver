410eb0476fb09a9d33a6a6dd6a274f90896fe72b
ASzwarc <szwarc.adam@gmail.com>
2019-09-24 08:52:35 +0200

    fix: corrected file formatting and named some parameters.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 from typing import List
 
+
 def main(filename: str) -> None:
     img_gray = cv.imread(filename, cv.IMREAD_GRAYSCALE)
     img_color = cv.imread(filename, cv.IMREAD_UNCHANGED)
@@ -12,13 +13,15 @@ def main(filename: str) -> None:
     else:
         print(f"Couldn't open image {filename} !")
 
+
 def threshold_image(image):
     max_val: int = 255
     return cv.adaptiveThreshold(image, max_val, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
                                 cv.THRESH_BINARY, 11, 2)
 
+
 def find_lines(image_grayscale, image_color):
-    def restructure_array(lines: List) -> List:
+    def restructure_array(lines) -> List:
         result = []
         for line in lines:
             x1, y1, x2, y2 = line[0]
@@ -49,17 +52,28 @@ def find_lines(image_grayscale, image_color):
         y2 = boundaries[3][1]
 
         return image_grayscale[x1:x2, y1:y2]
-    #TODO name this parameters
-    edges = cv.Canny(image_grayscale, 200, 220, apertureSize=3)
-    lines = cv.HoughLinesP(edges, 1, np.pi/180.0, 200, 300, 4)
 
-    draw_boundary(find_boundaries(restructure_array(lines)))
+    lowThreshold: int = 200
+    highThreshold: int = 220
+    rho: int = 1
+    theta: int = np.pi/180
+    threshold: int = 200
+    minLineLength: int = 300
+    maxLineGap: int = 4
+    edges = cv.Canny(image_grayscale, lowThreshold, highThreshold,
+                     apertureSize=3)
+    lines = cv.HoughLinesP(edges, rho, theta, threshold, minLineLength,
+                           maxLineGap)
+
+    draw_boundary(find_boundaries(lines))
     plot_image(cv.cvtColor(image_color, cv.COLOR_BGR2RGB), None)
     return crop_image(find_boundaries(restructure_array(lines)))
+
 
 def plot_image(image, color: str) -> None:
     plt.imshow(image, cmap=color)
     plt.show()
+
 
 if __name__ == "__main__":
     main("input_images/SimpleBoard_1.PNG")
