5e8f2ab5a3f026b66c801e7935e10c845d25a560
ASzwarc <szwarc.adam@gmail.com>
2019-09-26 22:32:31 +0200

    feature: added function for loading MNIST dataset.

--- model.py
+++ model.py
@@ -1,6 +1,18 @@
 import tensorflow as tf
 
 
+# TODO: download mnist dataset
+# TODO: create Keras model
+# TODO: train and evaluate
+# TODO: save model weights in separate file
+
+def load_data():
+    mnist_dataset = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist_dataset.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    return x_train, y_train, x_test, y_test
+
+
 def main():
     if tf.test.gpu_device_name():
         print(f"Default GPU Device: {tf.test.gpu_device_name()}")
