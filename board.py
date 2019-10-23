238c38da901480ba587af387c4f3df9e1f24522d
ASzwarc <szwarc.adam@gmail.com>
2019-10-23 18:20:37 +0200

    feature: changed printing board to be more readable. Added example of sudoku board which will be used to write algorithm for solving it.

--- board.py
+++ board.py
@@ -52,6 +52,20 @@ class Board():
         """
         for row in range(len(self._board)):
             self._logger.debug("|".join(
-                [str(val) for val in self._board[row]]))
+                [str(val) if val > 0 else " " for val in self._board[row]]))
             if row < 8:
-                self._logger.debug("- " * 9)
+                self._logger.debug("-" * 18)
+
+
+if __name__ == "__main__":
+    board = Board(logging.DEBUG)
+    board.add_row([0, 0, 0, 2, 6, 0, 7, 0, 1])
+    board.add_row([6, 8, 0, 0, 7, 0, 0, 9, 0])
+    board.add_row([1, 9, 0, 0, 0, 4, 5, 0, 0])
+    board.add_row([8, 2, 0, 1, 0, 0, 0, 4, 0])
+    board.add_row([0, 0, 4, 6, 0, 2, 9, 0, 0])
+    board.add_row([0, 5, 0, 0, 0, 3, 0, 2, 8])
+    board.add_row([0, 0, 9, 3, 0, 0, 0, 7, 4])
+    board.add_row([0, 4, 0, 0, 5, 0, 0, 3, 6])
+    board.add_row([7, 0, 3, 0, 1, 8, 0, 0, 0])
+    board.pretty_print()
