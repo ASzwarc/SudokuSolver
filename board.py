3e2e475aa8504028be964acd27211c4938452a16
ASzwarc <szwarc.adam@gmail.com>
2019-10-28 20:47:13 +0100

    refactoring: function evaluate_point was splitted into three separate functions for evaluation of row, column and box. Initialization of solution board was also moved to separate function.

--- board.py
+++ board.py
@@ -7,6 +7,9 @@ class Board():
     """
     Class for holding already filled sudoku board elements.
     """
+
+    Point = namedtuple('Point', ['row', 'col', 'val'])
+
     def __init__(self, logging_level) -> None:
         """
         Empty initialises Board.
@@ -57,63 +60,84 @@ class Board():
             if row < 8:
                 self._logger.debug("-" * 18)
 
+    def _evaluate_col_constraint(self, solution_board, point):
+        found_points = []
+        for elem_no, elem in enumerate(solution_board[point.row]):
+            if len(elem) > 1 and point.val in elem:
+                acc_val = solution_board[point.row][elem_no].replace(
+                    point.val, '', 1)
+                if len(acc_val) == 1:
+                    found_points.append(
+                        self.Point(point.row, elem_no, acc_val))
+                    self._logger.debug("Column eval: new point [%s, %s] = %s",
+                                       point.row, elem_no, acc_val)
+                solution_board[point.row][elem_no] = acc_val
+        return found_points
+
+    def _evaluate_row_constraint(self, solution_board, point):
+        found_points = []
+        for row_iter in range(len(solution_board)):
+            elem = solution_board[row_iter][point.col]
+            if len(elem) > 1 and point.val in elem:
+                acc_val = elem.replace(point.val, '', 1)
+                if len(acc_val) == 1:
+                    found_points.append(self.Point(
+                        row_iter, point.col, acc_val))
+                    self._logger.debug("Row eval: new point [%s, %s] = %s",
+                                       row_iter, point.col, acc_val)
+                solution_board[row_iter][point.col] = acc_val
+        return found_points
+
+    def _evaluate_box_constraint(self, solution_board, point):
+        found_points = []
+        for row_iter in range(0, len(solution_board), 3):
+            for col_iter in range(0, len(solution_board[0]), 3):
+                flattened = [elem for row in
+                             solution_board[row_iter:row_iter+3]
+                             for elem in row[col_iter:col_iter+3]]
+                for i, elem in enumerate(flattened):
+                    if len(elem) > 1 and point.val in elem:
+                        acc_val = elem.replace(point.val, '', 1)
+                        row = row_iter + (i // 3)
+                        col = col_iter + (i % 3)
+                        if len(acc_val) == 1:
+                            found_points.append(self.Point(row, col, acc_val))
+                            self._logger.debug(
+                                "Box eval: new point [%s, %s] = %s", row,
+                                col, acc_val)
+                        solution_board[row][col] = acc_val
+        return found_points
+
+    def _evaluate_point(self, solution_board, point):
+        found_points = []
+        found_points.extend(self._evaluate_col_constraint(
+            solution_board, point))
+        found_points.extend(self._evaluate_row_constraint(
+            solution_board, point))
+        found_points.extend(self._evaluate_box_constraint(
+            solution_board, point))
+        return found_points
+
+    def _init_solution_board(self):
+        initial_value = "123456789"
+        solution = [[initial_value for col in range(len(self._board[0]))]
+                    for row in range(len(self._board))]
+        found_points = []
+        for row_iter, row in enumerate(self._board):
+            for col_iter, elem in enumerate(row):
+                if elem != 0:
+                    solution[row_iter][col_iter] = str(elem)
+                    found_points.append(self.Point(
+                        row_iter, col_iter, str(elem)))
+        return solution, found_points
+
     def solver(self):
         def print_it_nicely(solution_board):
             print("\n")
             print("\n".join([" | ".join([elem.rjust(9, ' ') for elem in row])
                             for row in solution_board]))
 
-        def evaluate_point(solution_board, point):
-            found_points = []
-            for elem_no, elem in enumerate(solution_board[point.row]):
-                if len(elem) > 1 and point.val in elem:
-                    acc_val = solution_board[point.row][elem_no].replace(
-                        point.val, '', 1)
-                    if len(acc_val) == 1:
-                        found_points.append(Point(point.row, elem_no, acc_val))
-                        self._logger.debug("Row eval: new point [%s, %s] = %s",
-                                           point.row, elem_no, acc_val)
-                    solution_board[point.row][elem_no] = acc_val
-            for row_iter in range(len(solution_board)):
-                elem = solution_board[row_iter][point.col]
-                if len(elem) > 1 and point.val in elem:
-                    acc_val = elem.replace(point.val, '', 1)
-                    if len(acc_val) == 1:
-                        found_points.append(Point(
-                            row_iter, point.col, acc_val))
-                        self._logger.debug(
-                            "Column eval: new point [%s, %s] = %s",
-                            row_iter, point.col, acc_val)
-                    solution_board[row_iter][point.col] = acc_val
-            for row_iter in range(0, len(solution_board), 3):
-                for col_iter in range(0, len(solution_board[0]), 3):
-                    flattened = [elem for row in
-                                 solution_board[row_iter:row_iter+3]
-                                 for elem in row[col_iter:col_iter+3]]
-                    for elem in flattened:
-                        if len(elem) > 1 and point.val in elem:
-                            acc_val = elem.replace(point.val, '', 1)
-                            row = row_iter + (elem_no // 3)
-                            col = col_iter + (elem_no % 3)
-                            if len(acc_val) == 1:
-                                found_points.append(Point(row, col, acc_val))
-                                self._logger.debug(
-                                    "Box eval: new point [%s, %s] = %s", row,
-                                    col, acc_val)
-                            solution_board[row][col] = acc_val
-            return found_points
-
-        initial_value = "123456789"
-        solution = [[initial_value for col in range(len(self._board[0]))]
-                    for row in range(len(self._board))]
-        Point = namedtuple('Point', ['row', 'col', 'val'])
-        found_points = []
-        for row_iter, row in enumerate(self._board):
-            for col_iter, elem in enumerate(row):
-                if elem != 0:
-                    solution[row_iter][col_iter] = str(elem)
-                    found_points.append(Point(row_iter, col_iter, str(elem)))
-
+        solution, found_points = self._init_solution_board()
         print_it_nicely(solution)
         self._logger.debug("Found points evaluation")
         for point in found_points:
